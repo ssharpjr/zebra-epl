7cb946631457d4bd120186ca7e0a49356e74de1e
Stacey Sharp <ssharpjr@gmail.com>
2016-03-22 23:13:13 -0400

    Added comments and TODOS

--- grupo/grupolabel.py
+++ grupo/grupolabel.py
@@ -9,7 +9,7 @@ import sys
 from subprocess import check_output, STDOUT
 
 from time import time, strftime
-# import RPi.GPIO as GPIO
+import RPi.GPIO as io
 
 # TODO: Setup RTC
 # TODO: Setup LCD
@@ -20,29 +20,44 @@ from time import time, strftime
 # TODO: Verify the switch is present (at least one input is on)
 # TODO: Verify the button is present (check the light?)
 
-# Assign switch pins
-# sw_pos1_pin = 23
-# sw_pos2_pin = 24
-# sw_pos3_pin = 25
+# Assign RTC pins
+# rtc_clock_pin = ''
+# rtc_data_pin = ''
 
-# Setup GPIO
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(sw_pos1_pin, GPIO.IN)
-# GPIO.setup(sw_pos2_pin, GPIO.IN)
-# GPIO.setup(sw_pos3_pin, GPIO.IN)
+# Assign LCD pins
+# 4-6 pins here
+
+# Assign switch pins
+sw_pos1_pin = 23
+sw_pos2_pin = 24
+sw_pos3_pin = 25
+
+# Setup GPIO, pull-down resistors (False)
+io.setmode(io.BCM)
+io.setup(sw_pos1_pin, io.IN, pull_up_down=io.PUD_DOWN)
+io.setup(sw_pos2_pin, io.IN, pull_up_down=io.PUD_DOWN)
+io.setup(sw_pos3_pin, io.IN, pull_up_down=io.PUD_DOWN)
+
+
+def checkSwitch():
+    pass
 
 
 # Assign Part Number based on switch position
-# if (GPIO.input(sw_pos1_pin)):
-#     part_number = '403319PA'
-#     part_description = 'F15 ACS LID LH BSE WRN TRI BLK'
-# elif (GPIO.input(sw_pos2_pin)):
-#     part_number = 'partno2'
-#     part_description = 'partdesc2'
-# elif (GPIO.input(sw_pos3_pin)):
-#     part_number = 'partno3'
-#     part_description = 'partdesc3'
+def setPartNumber():
+    if (io.input(sw_pos1_pin)):
+        part_number = '403319PA'
+        part_description = 'F15 ACS LID LH BSE WRN TRI BLK'
+    elif (io.input(sw_pos2_pin)):
+        part_number = 'partno2'
+        part_description = 'partdesc2'
+    elif (io.input(sw_pos3_pin)):
+        part_number = 'partno3'
+        part_description = 'partdesc3'
+    return part_number, part_description
 
+
+# Defaults for testing
 part_number = '403319PA'
 part_description = 'F15 ACS LID LH BSE WRN TRI BLK'
 
@@ -68,7 +83,8 @@ def setSerialNumberTime(part_number):
 
 
 def printLabel():
-    printer = setPrinter()
+    # printer = setPrinter()  # Testing
+    printer = 'ZT230'
     serial_number, localtime = setSerialNumberTime(part_number)
     label = """N
 q406
@@ -80,16 +96,19 @@ A20,120,0,1,1,1,N,"S/N {sn}"
 A20,145,0,1,1,1,N,"{lt}"
 P1""".format(pn=part_number, pd=part_description, sn=serial_number,
              lt=localtime)
+
+    # Testing.  Will be os.system(command)
     print("lpr -P " + printer + " -o raw < " + label)
 
 
 def main():
-    setPrinter()
+    # setPrinter()
     printLabel()
 
 
 def exitProgram():
     print("Exiting")
+    io.cleanup()  # Clean up GPIO
     sys.exit()
 
 
